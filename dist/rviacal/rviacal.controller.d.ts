6550896f07b5abf3ab4c6f78e24bc304bbbb64e3
hechicero5000 <hechicero5000@gmail.com>
2024-11-26 07:26:13 -0600

    Delete dist directory

--- dist/rviacal/rviacal.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { RviacalService } from './rviacal.service';
-import { CreateRateProject } from './dto/create-rateproject.dto';
-import { CreateRateProjectIdDto } from './dto/dto-response';
-export declare class RviacalController {
-    private readonly rviacalService;
-    private readonly logger;
-    constructor(rviacalService: RviacalService);
-    addAppRateProject(id: number, createRateProject: CreateRateProject): Promise<CreateRateProjectIdDto>;
-}
